0a3e42a60144e8c798be00ad5243d8c973aefbdb
Coffeeteer <tgrieshiemer@gmail.com>
2016-08-08 17:31:41 -0400

    commented out 'delete burger'

--- server.js
+++ server.js
@@ -46,7 +46,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
