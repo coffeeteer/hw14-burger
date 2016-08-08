22d25815aa414540a1b18a82935984ac7561736f
Coffeeteer <tgrieshiemer@gmail.com>
2016-08-07 22:56:12 -0400

    server fixed but page not displaying on hostpage

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,39 @@
 // 3.body-parser
 
 //Create a router for the app, and export the router at end of this file.
+/*
+Here is where you create all the functions that will do the routing for your app, and the logic of each route.
+*/
+var express = require('express');
+var router = express.Router();
+var cat = require('../models/burgers.js');
+
+router.get('/', function (req, res) {
+	res.redirect('/burgers');
+});
+
+router.get('/cats', function (req, res) {
+	cat.all(function (data) {
+		var hbsObject = { burgers: data };
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
+});
+
+router.post('/cats/create', function (req, res) {
+	cat.create(['name', 'devoured'], [req.body.name, req.body.devoured], function () {
+		res.redirect('/burgers');
+	});
+});
+
+router.put('/cats/update/:id', function (req, res) {
+	var condition = 'id = ' + req.params.id;
+
+	console.log('condition', condition);
+
+	cat.update({ sleepy: req.body.sleepy }, condition, function () {
+		res.redirect('/burgers');
+	});
+});
+
+module.exports = router;
