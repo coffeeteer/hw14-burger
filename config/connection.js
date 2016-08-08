4c515bd8b3d423ca259bbdfa6c2d31724666b932
Coffeeteer <tgrieshiemer@gmail.com>
2016-08-07 20:44:44 -0400

    fixed the database and some of the other files

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,19 @@
 //setup code to connect Node to MySQL
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	port: 3306,
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'burgers_db'
+});
 
-module.exports = ; //export that code
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection; //export that code
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 -- Write insert querires to populate the burgers table with at least three entries.
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pizza Burger');
